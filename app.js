0579e8912f12792201332fcdc192a9dfe21db366
cfmitrah <kalidhasan.m@mitrahsoft.com>
2024-12-04 19:35:43 +0530

    first-commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! This is Doss MK. A Node.js app running in Docker127.\n');
+  res.end('Hello, This is Doss MK.\n');
 });
 
 server.listen(PORT, () => {
